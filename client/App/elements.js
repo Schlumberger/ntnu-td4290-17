529e9b3d0ee01f07b701ec7dceb67e1c53a08de6
Johannes Andersen <johanes1@online.no>
2017-09-05 19:00:42 +0200

    [Styling] Set grid-layout on App-component

---
+++ client/App/elements.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+/*
+  TODO: Import components and use them instead of divs
+  Example:
+
+  import Header from './Header';
+  export const HeaderMenu = styled(Header)`
+    grid-area: headerMenu;
+    background-color: red;
+    width: 100%;
+    height: 100%:
+  `;
+*/
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  grid-template-rows: 45px 1fr 50px;
+  grid-template-areas: 'headerMenu headerMenu' 'sideMenu   visualization'
+    'footer     footer';
+  height: 100vh;
+  width: 100vw;
+`;
+
+export const HeaderMenu = styled.div`
+  grid-area: headerMenu;
+  background-color: red;
+  width: 100%;
+  height: 100%:
+`;
+
+export const SideMenu = styled.div`
+  grid-area: sideMenu;
+  background-color: green;
+  width: 100%;
+  height: 100%:
+`;
+
+export const Visualization = styled.div`
+  grid-area: visualization;
+  background-color: blue;
+  width: 100%;
+  height: 100%:
+`;
+
+export const Footer = styled.div`
+  grid-area: footer;
+  background-color: yellow;
+  width: 100%;
+  height: 100%:
+`;
--- client/App/index.js
+++ client/App/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import Typ16 from 'components/Typ16';
+import {
+  Wrapper,
+  SideMenu,
+  HeaderMenu,
+  Visualization,
+  Footer
+} from './elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Typ16>
-        {'Hello World!'}
-      </Typ16>
+      <Wrapper>
+        <HeaderMenu />
+        <SideMenu />
+        <Visualization />
+        <Footer />
+      </Wrapper>
     );
   }
 }
