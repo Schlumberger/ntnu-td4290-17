48a3b5a2e0cd4e0383cbef7736996ee6af95118c
Johannes Andersen <johanes1@online.no>
2017-09-05 13:01:35 +0200

    [Initial Commit] Hosting and webpack running

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const isProduction = false;
+
+module.exports = function() {
+  return {
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    entry: ["./index.js"],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader"
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: "./index.html"
+      })
+    ],
+    devServer: isProduction
+      ? {}
+      : {
+          contentBase: path.resolve(__dirname, "dist")
+        }
+  };
+};
