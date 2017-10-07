f811ae5fd3d87da933fbedda29eedd04fac2e08a
Trudelutt <tjostad@hotmail.com>
2017-10-07 19:47:01 +0200

    add maxWidth test

--- tests/computeds/computeMaxDimentions.test.js
+++ tests/computeds/computeMaxDimentions.test.js
@@ -4,12 +4,20 @@ import { assert } from 'chai';
 
 import computed from 'computed/computeMaxDimentions';
 
-describe('Compute max dimensions', function() {
-  it('Should return maxWidth and maxHeight to 0', function() {
+describe('Compute max dimensions', function () {
+  it('Should return maxWidth and maxHeight to 0', function () {
     const result = runCompute(computed);
-    assert.deepEqual(result,  {
+    assert.deepEqual(result, {
       maxWidth: 0,
       maxHeight: 0
     });
+  });
+  it('Should find the correct maxHeigh', function () {
+    const result = runCompute(computed, {
+      state: {
+        formattedData: [{ points: [{ x: 10 }, { x: 20 }, { x: 5 }] }]
+      }
+    });
+    assert.deepEqual(result, { maxWidth: 20, maxHeight: 0 });
   });
 });
