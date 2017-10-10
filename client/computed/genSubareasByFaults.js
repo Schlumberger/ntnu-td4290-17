6b44129c0373238e66abb703b8d425328b294199
Matus Kosut <matuskosut@gmail.com>
2017-10-10 15:46:34 +0200

    [Feature] find opposite point of cut

--- client/computed/genSubareasByFaults.js
+++ client/computed/genSubareasByFaults.js
@@ -1,14 +1,14 @@
 /*
  * Returns undefined if intersection not found, otherwise {x, y} of that intersection point
  */
-function lineIntersectionPoint(id, point, fault) {
-  let {x: x1, y: y1} = fault.points[0];
-  let {x: x2, y: y2} = fault.points[1];
+function lineIntersectionPoint(id, point, fault, force=false) {
+  const {x: x1, y: y1} = fault.points[0];
+  const {x: x2, y: y2} = fault.points[1];
   // let {x: x3, x: x4, y0: y3, y1: y4} = point;
-  let {x3, x4, y3, y4} = point;
-  let y = ((x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4));
+  const {x3, x4, y3, y4} = point;
+  const y = ((x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4));
   // if (y > Math.max(y3, y4) || y < Math.min(y3, y4)) {
-  if (y > Math.max(y1, y2) || y < Math.min(y1, y2) || y > Math.max(y3, y4) || y < Math.min(y3, y4)) {
+  if ((y > Math.max(y1, y2) || y < Math.min(y1, y2) || y > Math.max(y3, y4) || y < Math.min(y3, y4)) && !force) {
     return undefined;
   } else {
     return {
@@ -17,6 +17,11 @@ function lineIntersectionPoint(id, point, fault) {
       id: id,
     }
   }
+}
+
+function getOppositeYPoint(isect, line) {
+  const fline = {points: [{x: isect.x, y: isect.y}, {x: isect.x, y: isect.y+10}]};
+  return lineIntersectionPoint(9, line, fline, true);
 }
 
 function pairwise(arr, func) {
@@ -38,6 +43,15 @@ function getFaultDirection(fault) {
 function directionCheck(value, cond) {
   if (value !== cond) {
     console.log('ERROR: wrong activity detected.');
+  };
+}
+
+function generateLinesForArea(act, next) {
+  return {
+    left: {x3: act.x, y3: act.y0, x4: act.x, y4: act.y1},
+    right: {x3: next.x, y3: next.y0, x4: next.x, y4: next.y1},
+    top: {x3: act.x, y3: act.y0, x4: next.x, y4: next.y0},
+    bottom: {x3: act.x, y3: act.y1, x4: next.x, y4: next.y1}
   };
 }
 
@@ -71,7 +85,7 @@ module.exports = (layers, faults) => {
         {id: layer.id+'-0', points: layer.points},
       ];
     };
-    console.log(layer);
+    // console.log(layer);
 
 
 
@@ -86,14 +100,9 @@ module.exports = (layers, faults) => {
         let rightArea = [];
         let wasCut = false; // if area was cut, other subareas will be pushed to the rightArea
 
-        console.log(subarea);
+        // console.log(subarea);
         pairwise(subarea.points, (act, next) => {
-          let lines = {
-            left: {x3: act.x, y3: act.y0, x4: act.x, y4: act.y1},
-            right: {x3: next.x, y3: next.y0, x4: next.x, y4: next.y1},
-            top: {x3: act.x, y3: act.y0, x4: next.x, y4: next.y0},
-            bottom: {x3: act.x, y3: act.y1, x4: next.x, y4: next.y1}
-          };
+          let lines = generateLinesForArea(act, next);
 
           let lineCuts = {};
           for (var key in lines) {
@@ -110,6 +119,8 @@ module.exports = (layers, faults) => {
             if (lineCuts.hasOwnProperty('left') && lineCuts.hasOwnProperty('top')) {
               directionCheck(faultDirection, 'right');
 
+              const oppositeYPoint = getOppositeYPoint(lineCuts['top'], lines['bottom']);
+              layer.intersections.push(oppositeYPoint);
               if (!wasCut) {
                 leftArea.push(act);
               } else {
@@ -119,6 +130,8 @@ module.exports = (layers, faults) => {
             } else if (lineCuts.hasOwnProperty('left') && lineCuts.hasOwnProperty('bottom')) {
               directionCheck(faultDirection, 'left');
 
+              const oppositeYPoint = getOppositeYPoint(lineCuts['bottom'], lines['top']);
+              layer.intersections.push(oppositeYPoint);
               if (!wasCut) {
                 leftArea.push(act);
               } else {
@@ -128,6 +141,8 @@ module.exports = (layers, faults) => {
             } else if (lineCuts.hasOwnProperty('right') && lineCuts.hasOwnProperty('top')) {
               directionCheck(faultDirection, 'left');
 
+              const oppositeYPoint = getOppositeYPoint(lineCuts['top'], lines['bottom']);
+              layer.intersections.push(oppositeYPoint);
               if (!wasCut) {
                 leftArea.push(act);
               } else {
@@ -137,6 +152,8 @@ module.exports = (layers, faults) => {
             } else if (lineCuts.hasOwnProperty('right') && lineCuts.hasOwnProperty('bottom')) {
               directionCheck(faultDirection, 'right');
 
+              const oppositeYPoint = getOppositeYPoint(lineCuts['bottom'], lines['top']);
+              layer.intersections.push(oppositeYPoint);
               if (!wasCut) {
                 leftArea.push(act);
               } else {
@@ -162,6 +179,13 @@ module.exports = (layers, faults) => {
                 rightArea.push(act);
               }
             } else if (lineCuts.hasOwnProperty('top') && lineCuts.hasOwnProperty('bottom')) {
+
+              const oppositeYPointL = getOppositeYPoint(lineCuts['bottom'], lines['top']);
+              layer.intersections.push(oppositeYPointL);
+
+              const oppositeYPointR = getOppositeYPoint(lineCuts['top'], lines['bottom']);
+              layer.intersections.push(oppositeYPointR);
+
               switch (faultDirection) {
                 case 'left':
                   // Two additional points
@@ -186,7 +210,7 @@ module.exports = (layers, faults) => {
             // TODO Detect new subareas based on intersections
 
           } else {
-            console.log('no intersections');
+            // console.log('no intersections');
             if (!wasCut) {
               leftArea.push(act);
             } else {
