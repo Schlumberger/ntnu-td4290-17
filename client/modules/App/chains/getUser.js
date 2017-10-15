719d082f961907ad83423fe8b147b150667e3d10
Martin Halvor Kostveit <martin_kostveit@hotmail.com>
2017-10-15 17:23:03 +0200

    [feature] authentication

--- client/modules/App/chains/getUser.js
+++ client/modules/App/chains/getUser.js
@@ -1,8 +1,15 @@
-import {getUser} from '@cerebral/firebase/operators';
+import {getUser, createUserWithEmailAndPassword, signInWithGoogle} from '@cerebral/firebase/operators';
 import {state, props} from 'cerebral/tags';
 import {set} from 'cerebral/operators';
 
 export default [
-    getUser(),
+    getUser(), {
+        error: [
+            createUserWithEmailAndPassword(email, password)
+        ]
+    },
+    signInWithGoogle({
+        redirect: false,
+    }),
     set(state`app.user`, props`response`)
 ]
