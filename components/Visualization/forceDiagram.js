9c5f470fb3da5c0c7a273e8228739b23e10cc94e
Eirik <eirik1502@gmail.com>
2017-10-27 19:05:11 +0200

    add links between layers ontop of each other. Stopped simulation for now, ontop links are red

--- components/Visualization/forceDiagram.js
+++ components/Visualization/forceDiagram.js
@@ -74,9 +74,9 @@ export const update = (el, props, state) => {
   // create simulation links
   const links = createLinksByNodes(nodes, xScale, yScale);
 
-  console.log(
-    'subareas skipped: ' + (subareas.length - nodes.length).toString()
-  );
+  // console.log(
+  //   'subareas skipped: ' + (subareas.length - nodes.length).toString()
+  // );
 
   const updateSubareas = svg
     .select('g#subareas')
@@ -117,8 +117,8 @@ export const update = (el, props, state) => {
     .enter()
     .append('line')
     .attr('class', 'link')
-    .attr('stroke', '#999')
-    .attr('stroke-width', '1.5px');
+    .attr('stroke', d => `${d.strength === -1 ? '#999' : '#f00'}`)
+    .attr('stroke-width', d => `${d.strength === -1 ? 1 : 0.5}px`);
 
   // create visual nodes
   const node = svg
@@ -155,7 +155,12 @@ export const update = (el, props, state) => {
       .attr('y2', d => d.target.y);
   };
 
-  createSimulation(nodes, links, simTick);
+  const sim = createSimulation(nodes, links, simTick);
+
+  // ________________REMOVE TO PLAY SIMULATION
+  sim.stop();
+  simTick(null); // to actually draw links
+  // _________________
 };
 
 const createSimulation = (nodes, links, tickFunc) => {
@@ -191,7 +196,7 @@ const createSimulation = (nodes, links, tickFunc) => {
         forceManyBody()
           .strength(d => {
             const f = areaForceScale(d.area); // negative to repel
-            console.log(f);
+            // console.log(f);
             return -f;
           })
           .distanceMax(d => areaForceDistScale(d))
@@ -201,7 +206,7 @@ const createSimulation = (nodes, links, tickFunc) => {
         forceLink(links)
           .id(d => d.id)
           .distance(d => d.prefDist) // to make nodes attract each other to a prefferred distance
-          .strength(d => 0.5)
+          .strength(d => (d.strength === -1 ? 0.5 : d.strength))
       )
       .on('tick', tickFunc)
   );
@@ -234,6 +239,18 @@ const createNodes = (subareas, xScale, yScale) => {
 };
 
 const createLinksByNodes = (nodes, xScale, yScale) => {
+  const links = createSublayerConnectorLinksByNodes(
+    nodes,
+    xScale,
+    yScale
+  ).concat(createOntopLinksByNodes(nodes, xScale, yScale));
+
+  return links;
+};
+
+// Creates links between "neighbouring" sublayers that belong to the same layer.
+// Neighbouring sublayers doesnt need to be intersecting
+const createSublayerConnectorLinksByNodes = (nodes, xScale, yScale) => {
   const links = [];
   const nodeCategories = {};
 
@@ -252,36 +269,88 @@ const createLinksByNodes = (nodes, xScale, yScale) => {
       const tarLen = tarN.points[tarN.points.length - 1].x - tarN.points[0].x;
       const distBetween = xScale(Math.abs(srcLen * 0.5 + tarLen * 0.5));
 
-      links.push({
-        source: srcN.id,
-        target: tarN.id,
-        prefDist: distBetween
-      });
+      links.push(createLink(srcN.id, tarN.id, distBetween, -1));
 
       nodeCategories[currNode.category].unshift(currNode);
     } else {
       nodeCategories[currNode.category] = [currNode];
     }
-
-    // let currSublayerName = nodes[i].id;
-    // let nextSublayerName = nodes[i + 1].id;
-    //
-    // // retrieve layer name without sublayer index
-    // let currLayerName = currSublayerName.substr(
-    //   0,
-    //   currSublayerName.lastIndexOf('-')
-    // );
-    // let nextLayerName = nextSublayerName.substr(
-    //   0,
-    //   nextSublayerName.lastIndexOf('-')
-    // );
-
-    // // create links between sublayers that belong to the same layer
-    // if (currLayerName === nextLayerName) {
-    //   links.push({ source: currSublayerName, target: nextSublayerName });
-    // }
   }
   return links;
+};
+
+// Create links based on precomputed information about what sublayers are on top of this one
+const createOntopLinksByNodes = (nodes, xScale, yScale) => {
+  const links = [];
+
+  nodes.forEach(n => {
+    // only look at nodes that has links to other nodes ontop
+    if (!n.hasOwnProperty('links')) return;
+
+    for (let l in n.links) {
+      // check if the property/key is defined in the object itself, not in parent
+
+      const srcN = n;
+      // find the node by name
+      let tarN = null;
+      for (let i in nodes) {
+        if (nodes[i].id === l) {
+          // console.log('node to be linked');
+          // console.log(nodes[i].id);
+          // console.log(l);
+
+          tarN = nodes[i];
+          break;
+        }
+      }
+
+      // if (tarN === null) {
+      //   console.log('didnt find target node of ontop link');
+      // }
+      // console.log(tarN);
+
+      // calculate the preferred distance between the layers
+
+      // find max height of the two nodes
+      const srcHeight = srcN.points.reduce(
+        (acc, p) => Math.max(acc, p.y1 - p.y0),
+        // p.hasOwnProperty('height') ? Math.max(acc, p.height) : acc,
+        0
+      ); // srcN.points[srcN.points.length - 1].x - srcN.points[0].x;
+      const tarHeight = tarN.points.reduce(
+        (acc, p) => Math.max(acc, p.y1 - p.y0),
+        // p.hasOwnProperty('height') ? Math.max(acc, p.height) : acc,
+        0
+      );
+      const heightBetween = xScale(Math.abs(srcHeight * 0.5 + tarHeight * 0.5));
+
+      // remove links that are between layers far apart (shouldnt be necessary if ontop layers are calculated correctly)
+      // if (Math.abs(tarN.y - srcN.y) > heightBetween * 2) {
+      //   break;
+      // }
+
+      // links.push({
+      //   source: srcN.id,
+      //   target: tarN.id,
+      //   prefDist: heightBetween
+      // });
+
+      const lin = createLink(n.id, l, heightBetween, 0);
+      // console.log(lin);
+      links.push(lin);
+    }
+  });
+
+  return links;
+};
+
+const createLink = (src, tar, dist, strength) => {
+  return {
+    source: src,
+    target: tar,
+    prefDist: dist,
+    strength: strength
+  };
 };
 
 export const destroy = el => {
